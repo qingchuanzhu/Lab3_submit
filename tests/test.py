5e2f93d06000836e291fbd9877ea75ff42036803
Qingchuan Zhu <zqc@utexas.edu>
2015-11-06 14:29:55 -0600

    remove false assert from test.py

--- tests/test.py
+++ tests/test.py
@@ -15,7 +15,6 @@ class StandAloneTests(TestCase):
         expected_result = ("a.com", "b.com")
 
 	self.assertEqual(computed_result, expected_result)
-	self.assertEqual(0,1)
         #assert computed_result == expected_result
 
 
